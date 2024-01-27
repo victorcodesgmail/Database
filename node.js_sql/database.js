76c2fffd5b88433fc9211efbdb07f55e1691696f
victorcodesgmail <victorcodes9532@gmail.com>
2024-01-27 20:28:55 +0300

    took data from login and stored in realtime firebase database

---
+++ node.js_sql/database.js
@@ -0,0 +1,20 @@
+const {createPool
+
+} = require('mysql');
+
+
+const pool =createPool({
+   host:"localhost",
+   user:"root",
+   password:"",
+   database:"test",
+   connectionLimit:10,
+
+})
+
+pool.query('select * from registration', (err, result, fields) => {
+   if (err){
+      return console.log(err)
+   }
+   return console.log(result);
+})
